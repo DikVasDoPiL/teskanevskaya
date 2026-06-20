3cab062bcb78e5260aa7a1a27bcb2222f1afcba9
dikvasdopil <it@firelli.ru>
2026-06-20 14:34:26 +0300

    fix styles

--- website/app/blueprints/main/services.py
+++ website/app/blueprints/main/services.py
@@ -4,16 +4,19 @@ from app.models import Product
 
 # Функция для получения N продуктов для каждой категории
 def get_products_per_category(n):
-    categories = Category.query.filter_by(active=True).all()
+    categories = Category.query.join(Category.products).distinct().all()
     result = dict()
     
     for category in categories:
+        cat_fields = {field.id: field.name for field in category.fields}
+
         if products := Product.query.filter_by(category_id=category.id).limit(n).all():
+
             result[category.name] = {
                 'products' : [product for product in products],
                 'image_path': category.image_path if category.image_path else None
             }
-    
+
     return result
 
 
